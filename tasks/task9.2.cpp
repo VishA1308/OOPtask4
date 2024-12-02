e605e40fdfdca74ecec3310e63b5417597c458d8
VishA1308 <a.vishnyakova1@g.nsu.ru>
2024-12-02 21:26:49 +0700

    Create task9.2.cpp

---
+++ tasks/task9.2.cpp
@@ -0,0 +1,95 @@
+#include <iostream>
+#include <string>
+
+class GenericCreature {
+public:
+    GenericCreature() {
+        std::cout << "GenericCreature: Constructor called." << std::endl;
+    }
+
+    ~GenericCreature() {
+        std::cout << "GenericCreature: Destructor called." << std::endl;
+    }
+};
+
+class OceanCreature : public GenericCreature {
+public:
+    OceanCreature() {
+        std::cout << "OceanCreature: Constructor called." << std::endl;
+    }
+
+    ~OceanCreature() {
+        std::cout << "OceanCreature: Destructor called." << std::endl;
+    }
+
+    void swim() {
+        std::cout << "OceanCreature: Swimming." << std::endl;
+    }
+};
+
+class Amphibious : public OceanCreature {
+public:
+    Amphibious() {
+        std::cout << "Amphibious: Constructor called." << std::endl;
+    }
+
+    ~Amphibious() {
+        std::cout << "Amphibious: Destructor called." << std::endl;
+    }
+
+    void walk() {
+        std::cout << "Amphibious: Walking." << std::endl;
+    }
+};
+
+class TerrestrialCreature : public  GenericCreature {
+public:
+    TerrestrialCreature() {
+        std::cout << "TerrestrialCreature: Constructor called." << std::endl;
+    }
+
+    ~TerrestrialCreature() {
+        std::cout << "TerrestrialCreature: Destructor called." << std::endl;
+    }
+    void walk() {
+        std::cout << "TerrestrialCreature: Walking." << std::endl;
+    }
+};
+
+class Bird : public TerrestrialCreature {
+public:
+    Bird() {
+        std::cout << "Bird: Constructor called." << std::endl;
+    }
+
+    ~Bird() {
+        std::cout << "Bird: Destructor called." << std::endl;
+    }
+
+    void fly() {
+        std::cout << "Bird: Flying." << std::endl;
+    }
+};
+
+class Waterfowl : public OceanCreature, public Bird{
+public:
+    Waterfowl() {
+        std::cout << "Waterfowl: Constructor called." << std::endl;
+    }
+
+    ~Waterfowl() {
+        std::cout << "Waterfowl: Destructor called." << std::endl;
+    }
+    
+    
+};
+
+int main() {
+    std::cout << "Creating a Waterfowl object:" << std::endl;
+    Waterfowl duck;
+    duck.walk(); // Из класса Bird (через TerrestrialCreature)
+    duck.fly(); // Из класса Bird
+    duck.swim(); // Из класса OceanCreature
+
+    return 0;
+}
